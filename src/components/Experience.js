4f185618f10ddb3d722e22ea92e6f9506006e023
AmanApT <amanasstudent@gmail.com>
2024-07-14 01:50:00 +0530

    Added React Dev Intern in Experience Section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -29,19 +29,48 @@ const Experience = ({ isLightTheme }) => {
           <section>
             <i class="icon fas fa-home"></i>
             <div class="details">
-              <span class="title">Freelancer</span>
-              <span>Since November 2022</span>
+              <span class="title">React Developer Intern @ iDesign.Market</span>
+              <span>Since April 2024</span>
             </div>
             <p>
-              I've been working as a freelancer since Nov'22. As a freelancer, I have the flexibility to choose my own clients and projects, which allows me to work on things that I am passionate about. 
+              Developed the Minutes of Meeting feature, enabling sharing, PDF
+              downloads, drafting, editing, and adding attachments,reducing
+              documentation time by 30 percent. Optimized API calls and state
+              management, cutting loading time by 40 percent
             </p>
+            {/* <p>
+            Managed the Item Masterfeature, enhancing item details and sub-item
+            organization, improving data retrieval by 25 percent
+            </p> */}
             <div class="bottom">
-              <a href="https://www.freelancer.in/u/amanpathak26">Freelance Profile</a>
+              <a href="https://www.idesign.market/" target="_blank">
+                iDesign.Market
+              </a>
               {/* <i>- Someone famous</i> */}
             </div>
           </section>
         </div>
         <div data-aos="fade-down" class="row row-2">
+          <section>
+            <i class="icon fas fa-home"></i>
+            <div class="details">
+              <span class="title">Freelancer</span>
+              <span>Since November 2022</span>
+            </div>
+            <p>
+              I've been working as a freelancer since Nov'22. As a freelancer, I
+              have the flexibility to choose my own clients and projects, which
+              allows me to work on things that I am passionate about.
+            </p>
+            <div class="bottom">
+              <a href="https://www.freelancer.in/u/amanpathak26"  target="_blank">
+                Freelance Profile
+              </a>
+              {/* <i>- Someone famous</i> */}
+            </div>
+          </section>
+        </div>
+        <div data-aos="fade-down" class="row row-1">
           <section>
             <i class="icon fas fa-star"></i>
             <div class="details">
@@ -49,10 +78,13 @@ const Experience = ({ isLightTheme }) => {
               <span>Dec 2022-Feb 2023</span>
             </div>
             <p>
-             Contributed to few projects at VigyaAi. One of them includes Profile Picker that provides a very effective solutions to the corporates for filtering their tonnes of cluttered resumes in just few clicks. 
+              Contributed to few projects at VigyaAi. One of them includes
+              Profile Picker that provides a very effective solutions to the
+              corporates for filtering their tonnes of cluttered resumes in just
+              few clicks.
             </p>
             <div class="bottom">
-              <a href="https://www.vigyaai.com/">VigyaAi</a>
+              <a href="https://www.vigyaai.com/"  target="_blank">VigyaAi</a>
               {/* <i>- Someone famous</i> */}
             </div>
           </section>
@@ -60,7 +92,7 @@ const Experience = ({ isLightTheme }) => {
         <div id="stars-group-1"></div>
         <div id="stars-group-2"></div>
         <div id="stars-group-3"></div>
-        <div data-aos="fade-down" class="row row-1">
+        <div data-aos="fade-down" class="row row-2">
           <section>
             <i class="icon fas fa-rocket"></i>
             <div class="details">
@@ -68,10 +100,36 @@ const Experience = ({ isLightTheme }) => {
               <span>Jan 2023</span>
             </div>
             <p>
-              Got a stunning position in the International Hackathon organised by Lablab.ai team. We built Machine Learning Based Project that uses Cohere API. The final product made us land in top 30 of the hackathon.
+              Got a stunning position in the International Hackathon organised
+              by Lablab.ai team. We built Machine Learning Based Project that
+              uses Cohere API. The final product made us land in top 30 of the
+              hackathon.
             </p>
             <div class="bottom">
-              <a href="https://lablab.ai/u/@AmanAp/cld34glda001ade0seyrvzix2"> Certification</a>
+              <a  target="_blank" href="https://lablab.ai/u/@AmanAp/cld34glda001ade0seyrvzix2">
+                {" "}
+                Certification
+              </a>
+              {/* <i>- Someone famous</i> */}
+            </div>
+          </section>
+        </div>
+        <div data-aos="fade-down" class="row row-1">
+          <section>
+            <i class="icon fas fa-globe"></i>
+            <div class="details">
+              <span class="title">1st Prize in WebSpin Contest</span>
+              <span>April 2022</span>
+            </div>
+            <p>
+              I got the first prize in an event that focused on Web Development.
+              The Event was on intercollege level with more than 100
+              participants.
+            </p>
+            <div class="bottom">
+              <a href="https://drive.google.com/file/d/1_nlIGzzrlNOR1WNNfCe9-I4S2wQMEPn_/view?usp=share_link">
+                Certification
+              </a>
               {/* <i>- Someone famous</i> */}
             </div>
           </section>
@@ -80,30 +138,17 @@ const Experience = ({ isLightTheme }) => {
           <section>
             <i class="icon fas fa-globe"></i>
             <div class="details">
-              <span class="title">1st Prize in WebSpin Contest</span>
-              <span>April 2022</span>
-            </div>
-            <p>
-              I got the first prize in an event that focused on Web Development. The Event was on intercollege level with more than 100 participants.
-            </p>
-            <div class="bottom">
-              <a href="https://drive.google.com/file/d/1_nlIGzzrlNOR1WNNfCe9-I4S2wQMEPn_/view?usp=share_link">Certification</a>
-              {/* <i>- Someone famous</i> */}
-            </div>
-          </section>
-        </div>
-        <div data-aos="fade-down" class="row row-1">
-          <section>
-            <i class="icon fas fa-globe"></i>
-            <div class="details">
               <span class="title">9th State Rank in IRAO</span>
               <span>February 2018</span>
             </div>
             <p>
-              I topped my district in International Reasoning Olympiad and also got 9th State Rank.
+              I topped my district in International Reasoning Olympiad and also
+              got 9th State Rank.
             </p>
             <div class="bottom">
-              <a href="https://drive.google.com/file/d/1_mzkZsVL8DqlML_-oLIKFjRFrZ1vaWUA/view?usp=share_link">Certification</a>
+              <a href="https://drive.google.com/file/d/1_mzkZsVL8DqlML_-oLIKFjRFrZ1vaWUA/view?usp=share_link">
+                Certification
+              </a>
               {/* <i>- Someone famous</i> */}
             </div>
           </section>
